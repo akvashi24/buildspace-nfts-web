b6a4b300690b144be5de511b4dcf9305e6c5da30
Adin Vashi <akvashi24@gmail.com>
2021-12-24 17:17:48 -0500

    add styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'gradient': 'carousel 4s ease infinite',
+      },
+      keyframes: {
+        'carousel': {
+          '0%': { 'background-position': "0% 50%" },
+          '50%': { 'background-position': "50% 100%" },
+          '100%': { 'background-position': "0% 50%" },
+        }
+      }
+    },
   },
   plugins: [],
 }
